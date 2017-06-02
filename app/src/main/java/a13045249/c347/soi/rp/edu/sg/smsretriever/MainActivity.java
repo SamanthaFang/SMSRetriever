6fcaf98dccbcba93b06217df197b691f6dfc3c5f
SamanthaFang <13045249@myrp.edu.sg>
2017-06-02 21:46:11 +0800

    Hi

---
+++ app/src/main/java/a13045249/c347/soi/rp/edu/sg/smsretriever/MainActivity.java
@@ -0,0 +1,33 @@
+package a13045249.c347.soi.rp.edu.sg.smsretriever;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        Fragment f1 = new FragmentFirst();
+        ft.replace(R.id.frame1, f1);
+
+        ft.commit();
+
+
+    }
+}
